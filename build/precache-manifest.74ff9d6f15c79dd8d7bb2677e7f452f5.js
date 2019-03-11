440b56733e39372f0b87a99fb1c31698d442364e
Spencer Jack <spencer.jack@alaskaair.com>
2019-03-11 11:17:09 -0700

    build commit

---
+++ build/precache-manifest.74ff9d6f15c79dd8d7bb2677e7f452f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "7fcce1a3db35b59adcd0",
+    "url": "/static/js/main.9167eba4.chunk.js"
+  },
+  {
+    "revision": "9363fbfcd60895900a06",
+    "url": "/static/js/2.2ab79dd4.chunk.js"
+  },
+  {
+    "revision": "7fcce1a3db35b59adcd0",
+    "url": "/static/css/main.a869e2dd.chunk.css"
+  },
+  {
+    "revision": "1d8eaa1f0bca1fa5024ceb41e6781322",
+    "url": "/index.html"
+  }
+];
