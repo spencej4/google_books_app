f8735779d872422a63b3f6cef1ce36c1125a3ad7
Spencer Jack <spencer.jack@alaskaair.com>
2019-03-13 13:21:35 -0700

    updates styling

--- src/components/Saved/index.js
+++ src/components/Saved/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default class Saved extends React.Component {
     render(){
         return(
-            <div className='saved'>Some Saved Stuff</div>
+            <div className='saved'>Saved Books</div>
         )
     }
 }
